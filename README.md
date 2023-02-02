01f63cbfb8584dbe5cce6a0da355c6c371b73c1c
YuanXiaodong <1506160036@qq.com>
2023-02-02 15:01:34 +0800

    初始commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SlateLearn
+
+Developed with Unreal Engine 5
---
+++ Source/SlateLearn/SlateLearnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SlateLearnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLATELEARN_API ASlateLearnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
